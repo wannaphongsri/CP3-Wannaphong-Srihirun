d22207f9abbfdf173d47cea66f518e5d5d3a3f81
wannaphongsri <wannaphong.sri@gmail.com>
2021-01-08 16:52:32 +0700

    Create Exercise4_Wannaphong_S.py

---
+++ assignments/Exercise4_Wannaphong_S.py
@@ -0,0 +1,10 @@
+FS = 79.0  #Score Foundation English
+GS = 60.0  #Score General Business
+IS = 68.5  #Score Introduction to Computer Systems
+CS = 85.3  #Computer Programming
+
+print("---Your Score---")
+print("Foundation English : ",FS, "คะแนน")
+print("General Business : ",GS, "คะแนน")
+print("Introduction to Computer Systems : ",IS, "คะแนน")
+print("Computer Programming : ",CS, "คะแนน")
