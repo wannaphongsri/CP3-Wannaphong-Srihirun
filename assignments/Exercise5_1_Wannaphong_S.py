3d870f5e40b6e9bf6502a03dd38577668f51875b
wannaphongsri <74394698+wannaphongsri@users.noreply.github.com>
2021-01-11 16:45:04 +0700

    BorntoDev Exercise5

---
+++ assignments/Exercise5_1_Wannaphong_S.py
@@ -0,0 +1,6 @@
+Num1 = int(input("กรุณาใส่ค่าตัวเลขตัวที่ 1 : "))
+Num2 = int(input("กรุณาใส่ค่าตัวเลขตัวที่ 2 : "))
+print(Num1," + ",Num2," = ",Num1+Num2)
+print(Num1," - ",Num2," = ",Num1-Num2)
+print(Num1," * ",Num2," = ",Num1*Num2)
+print(Num1," / ",Num2," = ",int(Num1/Num2))
---
+++ assignments/Exercise5_2_Wannaphong_S.py
@@ -0,0 +1,3 @@
+Distance = int(input("กรุณากรอกระยะทาง : "))
+Time = int(input("กรุณากรอกเวลา : "))
+print("ความเร็วในการเคลื่อนที่ ",Distance/Time," km/h")
